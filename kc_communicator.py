eb10a116bcf80b794e7a0f48c158b97f0642608b
Max Waine <bobishmax@hotmail.com>
2024-09-17 16:29:57 +1200

    Add current work

---
+++ kc_communicator.py
@@ -0,0 +1,131 @@
+#
+# Kruiz Control WebSocket Communicator
+# Copyright (c) 2024 Max Waine
+#
+# This software is distributed under the terms of the Free Software Foundation's
+# GNU GPLv2+. The full text can be found in LICENSE, which should be included with this
+# program, or at https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html#SEC1.
+#
+
+import sys
+import os
+import time
+import subprocess
+from functools import reduce
+from typing import Any
+
+import veadomini_websocket
+
+try:
+    from obswebsocket import obsws, exceptions, requests, events
+except:
+    print("You are missing obs-websocket-py. To install run 'pip install obs-websocket-py'")
+    sys.exit(1)
+
+#
+# This function exists because obswebsocket expects a response,
+# which is something we're not going to get for a custom event.
+# To work around this we temporarily set the timeout to basically nothing,
+# call with the BroadcastCustomEvent request, catch the timeout exception,
+# and reset timeout.
+#
+def send_custom_event(data: dict[str, Any]):
+    timeout = ws.timeout
+    ws.timeout = 0.001
+    try:
+        ws.call(requests.BroadcastCustomEvent(eventData=data))
+    except:
+        pass
+    ws.timeout = timeout
+
+#
+# Sends a message to Kruiz Control for consumption with OnOBSCustomMessage <message>.
+#
+def send_kruiz_control_message(message: str, data: str):
+    send_custom_event({'data': {'message': message, 'data': data}, 'realm': 'kruiz-control'})
+
+#
+# Handle an event with the 'kruiz-control' realm.
+#
+def on_kruiz_control_event(message):
+    event_message = message.getData()['message']
+    event_data    = message.getData()['data']
+
+    if event_message == 'TTS':
+        print("KC Event:\n\tMessage: {}\n\tData: {}".format(event_message, event_data))
+
+        event_data = event_data.replace('"', '\\"') # DO NOT LET END-USERS BREAK OUT OF THEIR STRING LITERAL PRISON.
+
+        subprocess.run('./sam.exe "{}"'.format(event_data))
+        send_kruiz_control_message('DoneWithSAM', 'SomeFeedbackData')
+    elif have_veadomini:
+        [instance, state] = event_data.split(' ')
+
+        instances = [instance] if instance != '*' else veadomini_websocket.instances
+
+        if event_message == 'VeadoMini_SetState':
+            change_state_method = veadomini_websocket.VeadoMiniInstance.set_state
+        elif event_message == 'VeadoMini_PushState':
+            change_state_method = veadomini_websocket.VeadoMiniInstance.push_state
+        elif event_message == 'VeadoMini_PopState':
+            change_state_method = veadomini_websocket.VeadoMiniInstance.pop_state
+        else:
+            return
+
+        for instance in instances:
+            change_state_method(veadomini_websocket.instances[instance], state)
+
+def foo(x, y):
+    print(x)
+    return os.path.exists(x) and os.path.exists(y)
+#
+# This is our hook for handling 'CustomEvent's, emitted by BroadcastCustomEvent.
+# If you want to handle any other custom events, you probably need to handle a new realm.
+#
+def on_custom_event(message):
+    if message.getRealm() == 'kruiz-control':
+        on_kruiz_control_event(message)
+
+#
+# Wow. It's main. Wow.
+#
+if __name__ == '__main__':
+    OBS_SETTING_PATHS = ('./obs/host.txt', './obs/port.txt', './obs/password.txt')
+
+    if all([os.path.exists(path) for path in OBS_SETTING_PATHS]):
+        host     = open('./obs/host.txt', 'r').read().strip()
+        port     = int(open('./obs/port.txt', 'r').read().strip())
+        password = open('./obs/password.txt', 'r').read().strip()
+    else:
+        if not os.path.exists('./obs'): os.makedirs('./obs')
+        for path in OBS_SETTING_PATHS:
+            open(path, 'w').close()
+        print("Please input your OBS settings into ./obs/host.txt, ./obs/port.txt, and ./obs/password.txt")
+        sys.exit(0)
+
+
+    have_veadomini = True
+    try:
+        veadomini_websocket.init()
+    except:
+        have_veadomini = False
+
+    # Try create the websocket.
+    print("Initialising OBS websocket")
+    ws = obsws(host, port, password, timeout=5)
+    ws.register(on_custom_event, events.CustomEvent)
+    try:
+        ws.connect()
+    except exceptions.ConnectionFailure as e:
+        print("Failed to connect to OBS websocket: {}".format(e))
+        sys.exit(1)
+    print("Successfully connected to OBS websocket")
+
+    # Spin until the user does a keyboard interrupt (usually Ctrl+C)
+    try:
+        while True:
+            time.sleep(1) # Pretty sure we could sleep forever but let's just sleep a second
+    except KeyboardInterrupt:
+        pass
+
+    ws.disconnect()
---
+++ veadomini_websocket.py
@@ -0,0 +1,106 @@
+#
+# veadotube-mini WebSocket Communicator
+# Copyright (c) 2024 Max Waine
+#
+# This software is distributed under the terms of the Free Software Foundation's
+# GNU GPLv2+. The full text can be found in LICENSE, which should be included with this
+# program, or at https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html#SEC1.
+#
+
+import os
+import json
+#import time
+
+#import asyncio
+#from websockets.asyncio.client import connect # as connect_async
+try:
+    from websockets.sync.client import connect # as connect_sync
+except:
+    print("You are missing websockets. To install run 'pip install websockets'")
+
+class VeadoMiniInstance:
+    def __init__(self, name: str, address: str):
+        self.websocket = connect(f"ws://{address}?n=Deervourer")
+        self.name      = name
+        self.address   = address
+        self.states    = self.get_states()
+
+    #
+    # Send stateEvents-type payload with an arg
+    #
+    def send_state_event_payload(self, event: str, arg_name: str, arg: str):
+        self.websocket.send(f'''
+            nodes:{{
+                "event": "payload",
+                "type": "stateEvents",
+                "id": "mini",
+                "payload": {{
+                    "event": "{event}",
+                    "{arg_name}": "{arg}"
+                }}
+            }}
+        '''.strip()) # Waste of resources but it makes the code less ugly, so who's to say if it's bad?
+        return self.websocket.recv()
+
+    #
+    # Send stateEvents-type payload with no args
+    #
+    def send_state_event_payload_no_args(self, event: str):
+        self.websocket.send(f'''
+            nodes:{{
+                "event": "payload",
+                "type": "stateEvents",
+                "id": "mini",
+                "payload": {{
+                    "event": "{event}"
+                }}
+            }}
+        '''.strip()) # Waste of resources but it makes the code less ugly, so who's to say if it's bad?
+        return self.websocket.recv()
+
+    #
+    # For a websocket get all of the states available and put them into a name->id dict.
+    #
+    def get_states(self) -> dict[str, int]:
+        states = {}
+
+        recv_data   = self.send_state_event_payload_no_args('list')
+        nodes       = recv_data[recv_data.find(':')+1:recv_data.rfind('}')+1]
+        nodes_json  = json.loads(nodes)
+        states_json = nodes_json["payload"]["states"]
+        for state in states_json:
+            states[state["name"]] = int(state["id"])
+
+        return states
+
+    #
+    def change_state(self, change_type: str, name: str):
+        self.send_state_event_payload(change_type, 'state', self.states[name])
+
+    def set_state(self, name: str):
+        self.send_state_event_payload('set', 'state', self.states[name])
+
+    def push_state(self, name: str):
+        self.send_state_event_payload('push', 'state', self.states[name])
+    
+    def pop_state(self, name: str):
+        self.send_state_event_payload('pop', 'state', self.states[name])
+
+VEADOMINI_LEN = len('veadotube mini - ')
+
+instances = {}
+
+def init():
+    instance_dir = os.path.expanduser('~/.veadotube/instances')
+
+    print("Initialising veadotube-mini websocket")
+    print("Available instance names:")
+    for file in os.listdir(instance_dir):
+        filename = os.fsdecode(file)
+
+        instance_json = json.loads(open(instance_dir + '/' + filename, 'r').read())
+        instance = VeadoMiniInstance(instance_json['name'][VEADOMINI_LEN:], instance_json['server'])
+        print(f" * {instance.name}")
+        instances[instance.name] = instance
+
+    print("Successfully connected to veadotube-mini websocket")
